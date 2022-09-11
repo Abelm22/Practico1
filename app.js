1e5f83b3af2ff5757720dee4ce6ab1aebc616435
Abelm22 <aag.montenegro@gmail.com>
2022-09-11 19:35:32 -0300

    Consigna 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function getCountryByIso3(isoCode){
     //Complete with your code
     const country = countries.find(country => country.iso3 === isoCode);
     return country;
-}
+};
 
 
 
@@ -20,7 +20,12 @@ function getCountryByIso3(isoCode){
  */
 function getCountryTranslatedName(isoCode, language){
     //Complete with your code
-}
+    const name = getCountryByIso3(isoCode) 
+    return name.translations[language];
+};
+
+
+
 /**Get an array of all the countries with the specified subregion
  * @param  {string} subregion
  * @returns {Array}
